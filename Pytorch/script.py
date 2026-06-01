caab9951b7573a8c28558867f36ee174106fbfba
Anirban Maitra <maitraanirban25@gmail.com>
2026-06-01 13:18:36 +0530

    Added Pytorch Autograd Notes

---
+++ Pytorch/script.py
@@ -0,0 +1,4 @@
+import nbformat
+with open("1_Pytorch_Tensors_Manipulation.ipynb", "r") as f:
+    nb = nbformat.read(f, as_version=4)
+nbformat.write(nb, "1_Pytorch_Tensors_Manipulation.ipynb", version=4)
