84c568e32212db44def4a5274c7274105c3c3ec4
Anirban Maitra <maitraanirban25@gmail.com>
2026-07-09 12:00:42 +0530

    ml folder

--- Pytorch/script.py
+++
@@ -1,4 +0,0 @@
-import nbformat
-with open("1_Pytorch_Tensors_Manipulation.ipynb", "r") as f:
-    nb = nbformat.read(f, as_version=4)
-nbformat.write(nb, "1_Pytorch_Tensors_Manipulation.ipynb", version=4)
